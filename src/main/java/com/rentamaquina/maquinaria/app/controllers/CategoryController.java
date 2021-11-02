aa2e79a32c9c03ef6ae1f1e195c492b9cf2191e9
NidyaC <91513740+NidyaC@users.noreply.github.com>
2021-11-02 18:22:50 -0500

    Crooss origin all

--- src/main/java/com/rentamaquina/maquinaria/app/controllers/CategoryController.java
+++ src/main/java/com/rentamaquina/maquinaria/app/controllers/CategoryController.java
@@ -27,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("Category")
+@CrossOrigin(origins = "*")
 public class CategoryController {
    
     @Autowired
